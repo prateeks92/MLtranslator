c49ee9bbdffd37004da62f4f19c479031c505205
Prateek Saxena <prateeksaxena@ufl.edu>
2017-04-28 06:03:43 -0400

    Update findNearest.py

--- findNearest.py
+++ findNearest.py
@@ -25,12 +25,3 @@ with open(spanishToWordMap, "r") as file:
 		keyArray=np.append(keyArray,line[1])
 
 print keyArray
-
-#array = np.random.random(10)
-
-# [ 0.21069679  0.61290182  0.63425412  0.84635244  0.91599191  0.00213826
-#   0.17104965  0.56874386  0.57319379  0.28719469]
-
-#value = 0.5
-
-#print(find_nearest(array, value))
